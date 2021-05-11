c9c44df00f6a8d25daf7dcc13d0d6c2affe4a6df
rohitbadarwal <rbadarwal@gmail.com>
2021-05-11 17:45:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const play = document.querySelector('#play');
+play.addEventListener('click', () => {
+    const p1Name = document.querySelector('#p1Name');
+    const p2Name = document.querySelector('#p2Name');
+    p1.name = p1Name.value;
+    p2.name = p2Name.value;
+});
+
+const p1 = {
+    name: "",
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    name: "",
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+            alert(`${player.name} won !!!`)
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+        p.name = '';
+        p1Name.value = '';
+        p2Name.value = '';
+    }
+}
